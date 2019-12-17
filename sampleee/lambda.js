4a283c34127a24e8d39e361fee670a6f2027034e
Indunil1992 <indunil@adroitlogic.com>
2019-12-17 12:41:49 +0530

    1

--- sampleee/lambda.js
+++ sampleee/lambda.js
@@ -5,14 +5,14 @@ exports.handler = async (event) => {
     translate.translateText({
         SourceLanguageCode: "ar",
         TargetLanguageCode: "en",
-        Text: "ummi"
+        Text: "um"
     }).promise()
         .then(data => {
             // your code goes here
             console.log(data);
         })
         .catch(err => {
-                        console.log(err);
+            console.log(err);
             // error handling goes here
         });
 
