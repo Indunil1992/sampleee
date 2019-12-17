1915c9101b926fd65f3c08fbe282826273caa65e
Indunil1992 <indunil@adroitlogic.com>
2019-12-17 13:05:28 +0530

    10

--- sampleee/lambda.js
+++ sampleee/lambda.js
@@ -1,39 +1,19 @@
 let AWS = require('aws-sdk');
-const s3 = new AWS.S3();
+const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-
-    s3.listObjects({
-        'Bucket': 'indunil.trigger',
-        'MaxKeys': 10,
-        'Prefix': ''
+    ddb.get({
+        TableName: "IndunilDDB",
+        Key: {
+            test: "10",
+            demo: "100"
+        }
     }).promise()
         .then(data => {
-            console.log(data);           // successful response
-                        console.log(data);
-
-            /*
-            data = {
-                Contents: [
-                    {
-                       ETag: "\"70ee1738b6b21e2c8a43f3a5ab0eee71\"",
-                       Key: "example1.jpg",
-                       LastModified: "<Date Representation>",
-                       Owner: {
-                          DisplayName: "myname",
-                          ID: "12345example25102679df27bb0ae12b3f85be6f290b936c4393484be31bebcc"
-                       },
-                       Size: 11,
-                       StorageClass: "STANDARD"
-                    },
-                    // {...}
-                ]
-            }
-            */
+            // your code goes here
         })
         .catch(err => {
-            console.log(err, err.stack); // an error occurred
-            console.log(err);
+            // error handling goes here
         });
 
     return { "message": "Successfully executed" };
